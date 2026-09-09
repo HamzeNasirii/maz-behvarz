e2dfe72aa83ef9c54e76b226bbac9bb8677876fc
Hamze Nasirii <Hamze_nasirii@yahoo.com>
2026-09-09 17:37:07 +0330

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static_src/'
+STATIC_URL = 'static/'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
